fda2976fbe7d7de5519d70f130b9f5c93b96f173
andyvuong <andyv.42@gmail.com>
2016-02-29 03:16:03 -0600

    add more styles and svg, add content

--- source_js/script.js
+++ source_js/script.js
@@ -1,1 +1,13 @@
 // Write any custom javascript functions here
+$(document).ready(function(){
+
+  $('.one-time').slick({
+      dots: true,
+      infinite: true,
+      speed: 400,
+      slidesToShow: 1,
+      prevArrow: "<img class='a-left control-c prev slick-prev' src='../data/pics/left-chevron.svg'>",
+      nextArrow: "<img class='a-right control-c next slick-next' src='../data/pics/right-chevron.svg'>"
+  });
+
+});
